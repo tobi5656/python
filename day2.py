7ddd8877298349b5ca8e555e37f63d6f55d293ab
Ojas Shinde <ojasss56@gmail.com>
2026-05-21 12:24:30 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,12 @@
+#day2
+a = "Ojas"
+print("hello \nI'm", a)
+x = complex(2, 3)
+print (x)
+print("the variable x is of type", type(x))
+print("the variable a is of type", type(a))
+
+name = input("Please enter your name: ")
+age = int(input("Please enter your age: "))
+canVote = age>=18
+print("Can you vote?", canVote)
